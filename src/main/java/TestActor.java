5b389b8e8733c878268924094002ae73f83c4544
Evgeniy <maximov.evgen@gmail.com>
2021-12-25 02:31:17 +0300

    Scripted auto-commit on change (2021-12-25 02:31:17) by gitwatch.sh

--- src/main/java/TestActor.java
+++ src/main/java/TestActor.java
@@ -1,7 +1,15 @@
 import akka.actor.AbstractActor;
 import akka.actor.ActorSelection;
 
+import javax.script.ScriptException;
+import java.util.ArrayList;
+
 public class TestActor extends AbstractActor {
     private ActorSelection storeActor = getContext().actorSelection("/user/storeActor");
     private final String SCRIPT_BY_NAME = "nashorn";
+
+    private ArrayList<Test> runTest(String jsScript, String functionName, String testName,
+                                    String expectedResult, ArrayList<Integer> params) throws ScriptException, NoSuchMethodException {
+        
+    }
 }
