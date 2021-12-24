cab4ab049c3a3146217fc415c230795c740d99bf
Evgeniy <maximov.evgen@gmail.com>
2021-12-25 02:30:39 +0300

    Scripted auto-commit on change (2021-12-25 02:30:39) by gitwatch.sh

--- src/main/java/TestActor.java
+++ src/main/java/TestActor.java
@@ -3,5 +3,5 @@ import akka.actor.ActorSelection;
 
 public class TestActor extends AbstractActor {
     private ActorSelection storeActor = getContext().actorSelection("/user/storeActor");
-    
+    private final String SCRIPT_BY_NAME = "nashorn";
 }
