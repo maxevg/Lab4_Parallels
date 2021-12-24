f88853225ff7025db8b9ef72f4b1babd232e0759
Evgeniy <maximov.evgen@gmail.com>
2021-12-25 02:14:02 +0300

    Scripted auto-commit on change (2021-12-25 02:14:02) by gitwatch.sh

--- src/main/java/TestPackageActor.java
+++ src/main/java/TestPackageActor.java
@@ -1,2 +1,12 @@
-public class TestPackageActor {
+import akka.actor.AbstractActor;
+import akka.actor.ActorSelection;
+import akka.japi.pf.ReceiveBuilder;
+
+public class TestPackageActor extends AbstractActor {
+    private ActorSelection testPerformerRouter = getContext().actorSelection("/user/testPerformerActor");
+
+    public Receive createReceive() {
+        return ReceiveBuilder.create()
+                .match(TestPackageMessage.class, )
+    }
 }
