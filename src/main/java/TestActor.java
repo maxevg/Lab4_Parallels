45191a05c5eb8f80518156584db027c7b7f5f2f1
Evgeniy <maximov.evgen@gmail.com>
2021-12-25 02:26:13 +0300

    Scripted auto-commit on change (2021-12-25 02:26:13) by gitwatch.sh

--- src/main/java/TestActor.java
+++ src/main/java/TestActor.java
@@ -1,2 +1,7 @@
-public class TestActor {
+import akka.actor.AbstractActor;
+import akka.actor.ActorSelection;
+
+public class TestActor extends AbstractActor {
+    private ActorSelection storeActor = getContext().actorSelection("/user/storeActor");
+    
 }
