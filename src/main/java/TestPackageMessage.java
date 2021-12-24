85a7e14038c890127a877cdc122af39dd4768dc8
Evgeniy <maximov.evgen@gmail.com>
2021-12-25 02:15:23 +0300

    Scripted auto-commit on change (2021-12-25 02:15:23) by gitwatch.sh

--- src/main/java/TestPackageMessage.java
+++ src/main/java/TestPackageMessage.java
@@ -1,4 +1,5 @@
 public class TestPackageMessage {
     private final String PACKAGE_ID = "packageId";
     private final String JS_SCRIPT = "jsScript";
+    private final String FUNCTION_NAME = "functionName";
 }
