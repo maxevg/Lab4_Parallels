693ae3585879bec4c8622c0fac59c446462e6541
Evgeniy <maximov.evgen@gmail.com>
2021-12-25 01:57:20 +0300

    Scripted auto-commit on change (2021-12-25 01:57:20) by gitwatch.sh

--- src/main/java/StoreMessage.java
+++ src/main/java/StoreMessage.java
@@ -1,9 +1,14 @@
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.ArrayList;
+
 public class StoreMessage {
     private final String PACKAGE_ID = "packageId";
     private final String TEST = "test";
 
     @JsonProperty(PACKAGE_ID)
     private Integer packageId;
+
+    @JsonProperty(TEST)
+    private ArrayList<Test> test;
 }
