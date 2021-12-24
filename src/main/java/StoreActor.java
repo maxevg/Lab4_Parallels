f9bf4e80cdf6e97e785002ee757ab7617c302b45
Evgeniy <maximov.evgen@gmail.com>
2021-12-25 01:55:33 +0300

    Scripted auto-commit on change (2021-12-25 01:55:33) by gitwatch.sh

--- src/main/java/StoreActor.java
+++ src/main/java/StoreActor.java
@@ -1,7 +1,15 @@
+import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-public class StoreActor {
+public class StoreActor extends AbstractActor {
     private Map<Integer, ArrayList<Test>> store = new HashMap<Integer, ArrayList<Test>>();
+
+    public Receive createReceive() {
+        return ReceiveBuilder.create()
+                .match()
+    }
 }
