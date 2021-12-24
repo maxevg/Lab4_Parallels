cda2c360f6efe5f86238fa491df9c507a84d6414
Evgeniy <maximov.evgen@gmail.com>
2021-12-25 01:29:13 +0300

    Scripted auto-commit on change (2021-12-25 01:29:13) by gitwatch.sh

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,7 +1,11 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Test {
     private final String TEST_NAME = "testName";
     private final String EXPECTED_RESULT = "expectedResult";
     private final String PARAMS = "params";
     private final String RESULT = "result";
-    
+
+    @JsonProperty(TEST_NAME)
+    private String testName;
 }
