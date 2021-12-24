76e6acbb87e3ac5bb626dc93e288bb9b1381e1e5
Evgeniy <maximov.evgen@gmail.com>
2021-12-25 01:16:05 +0300

    Scripted auto-commit on change (2021-12-25 01:16:05) by gitwatch.sh

--- src/main/java/WebServer.java
+++ src/main/java/WebServer.java
@@ -1,4 +1,7 @@
 public class WebServer {
     private static final String SERVER = "localhost";
     private static final Integer PORT = 8080;
+    private static final Integer NUMBER_OF_POOLS = 5;
+    private static final Integer TIME_OUT_MILLIS = 5000;
+    
 }
