bf1f0b7eca23265365106a734fb197779a064319
Evgeniy <maximov.evgen@gmail.com>
2021-12-25 02:58:07 +0300

    Scripted auto-commit on change (2021-12-25 02:58:07) by gitwatch.sh

--- src/main/java/WebServer.java
+++ src/main/java/WebServer.java
@@ -76,7 +76,9 @@ public class WebServer {
         );
         System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
         System.in.read();
-        
+        binding
+                .thenCompose(Tcp.ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate());
     }
 
 }
