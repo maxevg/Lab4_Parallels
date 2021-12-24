588926914eb2208feba49f1c39e46233dc286746
Evgeniy <maximov.evgen@gmail.com>
2021-12-25 01:20:04 +0300

    Scripted auto-commit on change (2021-12-25 01:20:04) by gitwatch.sh

--- src/main/java/WebServer.java
+++ src/main/java/WebServer.java
@@ -1,7 +1,18 @@
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+
 public class WebServer {
+    private ActorRef storeActor;
+    private final String STORE_ACTOR = "storeActor";
+
     private static final String SERVER = "localhost";
     private static final Integer PORT = 8080;
     private static final Integer NUMBER_OF_POOLS = 5;
     private static final Integer TIME_OUT_MILLIS = 5000;
-    
+
+    private WebServer(final ActorSystem system) {
+        storeActor = system.actorOf(Props.create(StoreActor.class) STORE_ACTOR)
+    }
+
 }
