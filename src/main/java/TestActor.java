a87d91023d9c55cc4e82e57c313cd1b87f50568a
Evgeniy <maximov.evgen@gmail.com>
2021-12-25 02:34:26 +0300

    Scripted auto-commit on change (2021-12-25 02:34:26) by gitwatch.sh

--- src/main/java/TestActor.java
+++ src/main/java/TestActor.java
@@ -1,6 +1,9 @@
 import akka.actor.AbstractActor;
 import akka.actor.ActorSelection;
 
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 import java.util.ArrayList;
 
@@ -10,6 +13,11 @@ public class TestActor extends AbstractActor {
 
     private ArrayList<Test> runTest(String jsScript, String functionName, String testName,
                                     String expectedResult, ArrayList<Integer> params) throws ScriptException, NoSuchMethodException {
-        
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName(SCRIPT_BY_NAME);
+        engine.eval(jsScript);
+        Invocable invocable = (Invocable) engine;
+        String result = invocable.invokeFunction(functionName, params.toArray()).toString();
+
+
     }
 }
