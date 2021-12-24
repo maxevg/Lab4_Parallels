57b5bf5252c72415779ef9d6d64775eb9241e754
Evgeniy <maximov.evgen@gmail.com>
2021-12-25 02:10:12 +0300

    Scripted auto-commit on change (2021-12-25 02:10:12) by gitwatch.sh

--- src/main/java/WebServer.java
+++ src/main/java/WebServer.java
@@ -9,6 +9,9 @@ public class WebServer {
     private ActorRef testPerformerActor;
     private final String TEST_PERFORMER_ACTOR = "testPerformerActor";
 
+    private ActorRef testPackageActor;
+    private final String TEST_PACKAGE_ACTOR = "testPackageActor";
+
     private static final String SERVER = "localhost";
     private static final Integer PORT = 8080;
     private static final Integer NUMBER_OF_POOLS = 5;
